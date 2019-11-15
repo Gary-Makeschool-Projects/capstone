4fe58363c9398988cbc942b7db604990f8c5e961
imthaghost <imthaghost@protonmail.com>
2019-11-14 22:16:00 -0800

    [update] heroku

--- app.py
+++ app.py
@@ -10,9 +10,20 @@ app = Flask(__name__)  # name the application
 @app.route('/index', methods=['GET'])
 def index():
     # render index page
-    return 'Hi for now'
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    # render login page
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    # render signup page
+    return render_template('signup.html')
 
 
 if __name__ == "__main__":
-    os.environ['FLASK_ENV'] = 'development'
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='0.0.0.0')
