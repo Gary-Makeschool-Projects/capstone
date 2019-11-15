0f679e1372ecf69e142b96b7eb640a64c6b2b7cc
imthaghost <imthaghost@protonmail.com>
2019-11-15 01:37:22 -0800

    [update] tracking

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template, render_template
-
+from flask import Flask, render_template, render_template, session, request, jsonify, redirect, send_from_directory
+from json import dumps
 
 app = Flask(__name__)  # name the application
 
@@ -9,8 +9,23 @@ app = Flask(__name__)  # name the application
 @app.route('/')
 @app.route('/index', methods=['GET'])
 def index():
-    # render index page
-    return render_template('index.html')
+    if request.method == 'GET':
+        if 'visitor' in session:
+            ip = session['visitor']
+            return render_template('index.html', ip=ip)
+
+        else:
+            data = {
+                'server_ip':  request.remote_addr,
+                'client_ip': request.environ.get('HTTP_X_FORWARDED_FOR'),
+                'real_ip': request.environ.get('HTTP_X_REAL_IP')
+
+            }
+            session['visitor'] = dumps(data)
+            ip = session['visitor']
+            return render_template('index.html', ip=ip)
+    else:
+        return redirect('index')
 
 
 @app.route('/login', methods=['GET'])
@@ -21,9 +36,14 @@ def login():
 
 @app.route('/signup', methods=['GET'])
 def signup():
+
     # render signup page
     return render_template('signup.html')
 
+    if request.method == 'POST':
+        return send_from_directory('static/', filename='terms.txt')
+
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
     app.run(debug=True, host='0.0.0.0')
